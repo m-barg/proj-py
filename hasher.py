5405a51bba410559cd4072226780a2bc059113a4
unknown <dbacot@gamingcampus.local>
2024-12-16 11:41:23 +0100

    modifs csv bdd

---
+++ hasher.py
@@ -0,0 +1,19 @@
+
+import csv
+import hashlib
+import pandas
+h = hashlib.new("SHA256")
+def nouveau_user():
+    nomuser = input("donne ton user")
+    vrai_mdp = input("donne ton mdp on va le haché grr paw")
+    h.update(vrai_mdp.encode())
+    hache_mdp = h.hexdigest()
+    print(hache_mdp)
+    with open('csvuser.csv','a', newline='', encoding='utf-8') as fichier :
+        writer = csv.writer(fichier)
+        writer.writerow([2,nomuser,hache_mdp])
+    return writer
+
+print(nouveau_user())
+
+
